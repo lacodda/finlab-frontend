9bdba8b96c23e167aa2697a9552baa0adcde9cbf
Kirill Lakhtachev <lahtachev@gmail.com>
2023-04-14 14:04:11 +0200

    ui: added LoadingBar (index)

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,5 +1,5 @@
-export * from './lib/ui';
 export * from './lib/elementary/Button';
 export * from './lib/elementary/Input';
 export * from './lib/elementary/Checkbox';
 export * from './lib/elementary/ModeSwitch';
+export * from './lib/elementary/LoadingBar';
